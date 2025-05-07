15744b2c85255d32c962a37c1db477261105db02
CaptainCellu <riccardo.bi05@gmail.com>
2025-05-07 19:45:11 +0200

    added bookbot.py, same as main.py but can be invoked directly from the console

---
+++ bookbot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+from stats import *
+
+#sort_num_char
+def main():
+    path = sys.argv[1]
+    sorted_list_of_num_char = sort_num_char(get_num_char(get_book_text(path)))
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(get_book_text(path))} total words")
+    print("--------- Character Count -------")
+
+    for element in sorted_list_of_num_char:
+        if element["char"].isalpha():
+            print(f"{element["char"]}: {element["num"]}")
+
+    print("============= END ===============")
+
+    return
+
+if len(sys.argv) == 2:
+    main()
+else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
